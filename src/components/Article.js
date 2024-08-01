21828f2f127f709f1f149aae375bbd546b4692be
Simon Nguthu <simon.nguthu@student.moringaschool.com>
2024-08-01 11:31:16 +0300

    Blog mini-project

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,22 @@
 import React from "react";
 
 function Article(props) {
+  let min = props.minutes
+  
+  const interval = min < 30 ? 5 : 10
+  const emoji = min < 30 ? "☕️" : "🍱" 
+  let emojis = ""
+  for (let i = 0; i < min; i += interval){
+    emojis += emoji
+  }
+
+  
   return (
     <article>
       <h3>{props.title}</h3>
-      <small>{props.date || "January 1, 1970"}</small>
+      <small>{props.date || "January 1, 1970"}
+        <span> {emojis} {min} min read</span>
+      </small>
       <p>{props.preview}</p>
     </article>
   );
