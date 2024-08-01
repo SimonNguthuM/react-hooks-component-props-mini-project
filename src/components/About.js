6d3526cd8d5795b7fc0d48f7240aaf1fa317f29f
Simon Nguthu <simon.nguthu@student.moringaschool.com>
2024-08-01 10:05:37 +0300

    Blog mini-project

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-function About(props) {
+function About({ image = "https://via.placeholder.com/215", about }) {
     return (
         <aside>
-            <img src ={props.image || "https://via.placeholder.com/215"} alt="blog logo"></img>
-            <p>{props.about}</p>
+            <img src ={image} alt="blog logo"></img>
+            <p>{about}</p>
         </aside>
     )
 }
